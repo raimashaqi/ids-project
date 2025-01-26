492707a7e7127e9b936104128240460f75325d54
xecureyan11 <1227050084@student.uinsgd.ac.id>
2025-01-26 08:03:14 +0700

    upd

--- app/example/myFig.py
+++
@@ -1,9 +0,0 @@
-from pyfiglet import Figlet
-
-def banner():
-    f = Figlet()
-    print(f.renderText("Threats_Detector"))
-    print("~# Author: PT. Yuk Mari Proyek Indonesia")
-    print("~# Copyright © 2025")
-
-banner()
