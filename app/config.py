76c1ab9d2f795cd1823409a2ab761d522f480a18
xecureyan11 <1227050084@student.uinsgd.ac.id>
2025-02-06 03:44:55 +0700

    update

--- app/config.py
+++ app/config.py
@@ -1,4 +1,9 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 class Config:
-    SECRET_KEY = 'amanbangetgess'
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:@localhost/yuk_mari'
+    SECRET_KEY = os.getenv('SECRET_KEY', 'amanbangetgess')
+    SQLALCHEMY_DATABASE_URI = f"mysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False 
