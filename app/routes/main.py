c229045b3855eefc115194482bc0055c00ad07ff
xecureyan11 <1227050084@student.uinsgd.ac.id>
2025-02-08 11:39:50 +0700

    import, account_settings

--- app/routes/main.py
+++ app/routes/main.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, flash, redirect, url_for
+from flask import Blueprint, render_template, flash, redirect, url_for, session
 from app.utils.decorators import login_required
 from app.models.log import Log
 
@@ -13,19 +13,25 @@ def index():
 @main_bp.route('/dashboard')
 @login_required
 def dashboard():
-    try:
-        logs = Log.query.order_by(Log.log_time.desc()).all()
-        return render_template('dashboard.html', logs=logs)
-    except Exception as e:
-        flash('Error mengambil data logs', 'danger')
-        return redirect(url_for('main.index'))
+    return render_template('dashboard.html')
 
 @main_bp.route('/export')
 @login_required
 def export():
-    try:
-        logs = Log.query.order_by(Log.log_time.desc()).all()
-        return render_template('export.html', logs=logs)
-    except Exception as e:
-        flash('Error mengambil data logs', 'danger')
-        return redirect(url_for('main.dashboard')) 
+    return render_template('export.html')
+
+@main_bp.route('/importt')
+@login_required
+def importt():
+    return render_template('importt.html')
+    # try:
+    #     logs = Log.query.order_by(Log.log_time.desc()).all()
+    #     return render_template('importt.html', logs=logs)
+    # except Exception as e:
+    #     flash('Error mengambil data logs', 'danger')
+    #     return redirect(url_for('main.dashboard')) 
+
+@main_bp.route('/account_settings')
+@login_required
+def account_settings():
+    return render_template('account_settings.html')
