0deaf404ba349d372b8a65755c2cd92d8c16871c
raimashaqi <shaqiraima@upi.edu>
2025-04-08 14:23:15 +0700

    updated

--- app/translations.py
+++ app/translations.py
@@ -14,3 +14,31 @@ translations = {
         'copyright': '© 2025 PT. Yuk Mari Project Indonesia'
     }
 } 
+
+dashboard ={
+    'id':{
+    "welcome": "Selamat Datang di Dashboard",
+    "threat_logs": "Ancaman Log",
+    "total_threats": "Total Ancaman Logs",
+    "severity_levels": "Ancaman Logs Berdasarkan Tingkat Keparahan",
+    "low": "Rendah",
+    "medium": "Sedang",
+    "high": "Tinggi",
+    "critical": "Genting",
+    "language": "Bahasa",
+    "developer_team": "Tim Pengembang"
+    },
+    'en':{
+    "welcome": "Welcome to the Dashboard",
+    "threat_logs": "Threat Logs",
+    "total_threats": "Total Threat Logs",
+    "severity_levels": "Threat Logs Based on Severity",
+    "low": "Low",
+    "medium": "Medium",
+    "high": "High",
+    "critical": "Critical",
+    "language": "Language",
+    "developer_team": "Developer Team"
+    }
+    
+}
