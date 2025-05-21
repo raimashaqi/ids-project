0d94153483403c415c64fe0e755547f912fbf27f
Raifal Bagus Afdiansah <62599204+afdiansah@users.noreply.github.com>
2025-05-21 14:18:11 +0700

    perbaikan struktur css

--- app/static/js/detector.js
+++ app/static/js/detector.js
@@ -5,8 +5,8 @@
 (function () {
   // Konfigurasi dasar
   const config = {
-    apiUrl: 'http://localhost:1337/test_payload',
-    // apiUrl: 'https://f1d3-114-10-45-131.ngrok-free.app/test_payload', // URL API untuk mendeteksi payload pastikan menggunakan ngrok Server
+    // apiUrl: 'http://localhost:1337/test_payload',
+    apiUrl: 'https://f1d3-114-10-45-131.ngrok-free.app/test_payload', // URL API untuk mendeteksi payload pastikan menggunakan ngrok Server
     debug: false,
     checkOnSubmit: true   // Periksa saat form disubmit
   };
